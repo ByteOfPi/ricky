78740cd78cd69ab774cbeeaf4ca5f4a1844094dd
Joseph Shim <joseph.shim@outlook.com>
2013-06-30 23:14:35 -0400

    Started the LED Control for ricky

---
+++ ricky.ino
@@ -0,0 +1,40 @@
+/*This program is made to run the LED lights connected to ricky.
+If additional functions are to be created for ricky it is reccommended
+that the user creates another file for the ricky AI structure.
+GLHF
+*/
+/* Pin Connections/Functionality
+Currently the car has 3 toggle switches to control the RGB lights
+The LED strips are all in parallel so we will treat them as one set each
+
+My reccomendation is that the 3 switches act as a 3bit binary input code
+
+000 = Off
+001 = Red
+010 = Green
+011 = Blue
+100 = Red+Green
+101 = Red+Blue
+110 = Green+Blue
+111 = WildStyle
+
+Pin09 = Red Wire
+Pin10 = Green Wire
+Pin11 = Blue Wire
+GND = GND
+
+Pin08 = Switch 1
+Pin12 = Switch 2
+Pin13 = Switch 3
+*/
+
+setup
+{
+  
+}
+
+loop
+{
+  
+  
+}
